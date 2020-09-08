6e4e3e10cfe2ac5ff129a2fc139b1a54d70ee757
4dragunov <4dragunov@gmail.com>
2020-09-08 23:23:34 +0300

    hw

--- homework.py
+++ homework.py
@@ -1,11 +1,17 @@
+import logging
 import os
-import requests
-import telegram
 import time
+
 from dotenv import load_dotenv
+
+import requests
+
+import telegram
 
 load_dotenv()
 
+logging.basicConfig(level=logging.ERROR, filename='app.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -13,34 +19,48 @@ CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему ' \
+                  'уроку.'
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+    try:
+        headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+        url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+        homework_statuses = requests.get(url, headers=headers,
+                                         params={
+                                             'from_date': current_timestamp})
+        return homework_statuses.json()
+    except Exception as e:
+        print(f'Не удалось проверить статус дз: {e}')
+        logging.error("Exception occurred", exc_info=True)
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    try:
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        return bot.send_message(chat_id=CHAT_ID, text=message)
+    except Exception as e:
+        print(f'Не удалось отправить сообщение: {e}')
+        logging.debug("Debug info", exc_info=True)
+        logging.error("Exception occurred", exc_info=True)
 
 
 def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
-
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]))
+            current_timestamp = new_homework.get(
+                'current_date')  # обновить timestamp
             time.sleep(300)  # опрашивать раз в пять минут
 
         except Exception as e:
